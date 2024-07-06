9a5fb709bb2a457229e5bfceaf2fdfd18578b2bb
abdoubsa <abdouboussaa19@gmail.com>
2024-07-06 13:45:14 +0100

    added instructions in the default page

--- index.js
+++ index.js
@@ -15,7 +15,21 @@ app.use("/api/characters", characterRoute);
 
 //
 app.get("/", (req, res) => {
-  res.json("default page");
+  res.json({
+    app_name: "Crud api for managing game characters ",
+    add_character:
+      "you can create a chararacter in postman using /api/characters/, the character has (characterName, characterRace, characterHP)",
+    view_all_characters:
+      "you can view all the characters in the database using /api/characters/all",
+    view_specific_character:
+      "you can veiw a specific character using /api/characters/:id",
+    update_character:
+      "you can update a character in postman using /api/characters/:id in a put request",
+    delete_character:
+      "you can delete a character in postman using /api/characters/:id in a delete request",
+    view_paginated:
+      "you can show a paginated view of certain characters list using /api/characters?page=<pageNumber>&limit=<number_of_characters_in_each_page>",
+  });
 });
 
 app.all("*", (req, res, next) => {
